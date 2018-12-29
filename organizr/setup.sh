70efb75b359678a45a766b059d20913d353478a8
Tom Stones <tom@tomstones.com.au>
2018-12-29 22:25:04 +1100

    UPDATED | fpm settings

--- organizr/setup.sh
+++ organizr/setup.sh
@@ -1,19 +1,15 @@
-# Create a temporary json config for iocage
-echo '{"pkgs":[ "nginx","git","wget","php70","php70-curl","php70-pdo","php70-sqlite3","php70-simplexml","php70-zip","php70-openssl","curl","php70-hash","php70-json","php70-session","php70-pdo_sqlite"]}' > /tmp/pkg.json
 # Create the jail
-iocage create -n "organizr" -p /tmp/pkg.json -r 11.2-RELEASE ip4_addr="vnet0|192.168.1.60/24" defaultrouter="192.168.1.1" vnet="on" allow_raw_sockets="1" boot="on" 
-# Remove the temporary configuration
-rm /tmp/pkg.json
+iocage create -n "organizr" -p ./pkg.json -r 11.2-RELEASE ip4_addr="vnet0|192.168.1.60/24" defaultrouter="192.168.1.1" vnet="on" allow_raw_sockets="1" boot="on" 
 
 # Add config folder
 iocage exec organizr mkdir -p /mnt/config
 iocage fstab -a organizr /mnt/Tank/configs/organizr /mnt/config nullfs rw 0 0
 
 # Update the php fpm config
-iocage exec organizr echo 'listen = /var/run/php-fpm.sock' >> /usr/local/etc/php-fpm.conf
-iocage exec organizr echo 'listen.owner = www' >> /usr/local/etc/php-fpm.conf
-iocage exec organizr echo 'listen.group = www' >> /usr/local/etc/php-fpm.conf
-iocage exec organizr echo 'listen.mode = 0660' >> /usr/local/etc/php-fpm.conf
+iocage exec organizr sed -i '' -e 's?listen = 127.0.0.1:9000?listen = /var/run/php-fpm.sock?g' /usr/local/etc/php-fpm.conf
+iocage exec organizr sed -i '' -e 's/;listen.owner = www/listen.owner = www/g' /usr/local/etc/php-fpm.conf
+iocage exec organizr sed -i '' -e 's/;listen.group = www/listen.group = www/g' /usr/local/etc/php-fpm.conf
+iocage exec organizr sed -i '' -e 's/;listen.mode = 0660/listen.mode = 0600/g' /usr/local/etc/php-fpm.conf
 
 # Update the PHP ini
 iocage exec organizr cp /usr/local/etc/php.ini-production /usr/local/etc/php.ini
@@ -21,7 +17,7 @@ iocage exec organizr sed -i '' -e 's?;date.timezone =?date.timezone = "Universal
 iocage exec organizr sed -i '' -e 's?;cgi.fix_pathinfo=1?cgi.fix_pathinfo=0?g' /usr/local/etc/php.ini
 
 # Pull down latest source
-iocage exec organizr git clone -b v2-develop https://github.com/causefx/Organizr /usr/local/www/Organizr
+iocage exec organizr git clone --depth=1 -b v2-develop https://github.com/causefx/Organizr /usr/local/www/Organizr
 
 # Set permissions
 iocage exec organizr chown -R www:www /usr/local/www /mnt/config
