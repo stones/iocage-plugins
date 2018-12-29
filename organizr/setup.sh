f5e135dee436e658647ee3ac15b21ddce24c45bd
Tom Stones <tom@tomstones.com.au>
2018-12-29 21:54:23 +1100

    UPDATED | Swapped console for multiple execs

--- organizr/setup.sh
+++ organizr/setup.sh
@@ -10,12 +10,10 @@ iocage exec organizr mkdir -p /mnt/config
 iocage fstab -a organizr /mnt/Tank/configs/organizr /mnt/config nullfs rw 0 0
 
 # Update the php fpm config
-iocage console organizr
-echo 'listen = /var/run/php-fpm.sock' >> /usr/local/etc/php-fpm.conf
-echo 'listen.owner = www' >> /usr/local/etc/php-fpm.conf
-echo 'listen.group = www' >> /usr/local/etc/php-fpm.conf
-echo 'listen.mode = 0660' >> /usr/local/etc/php-fpm.conf
-exit
+iocage exec organizr echo 'listen = /var/run/php-fpm.sock' >> /usr/local/etc/php-fpm.conf
+iocage exec organizr echo 'listen.owner = www' >> /usr/local/etc/php-fpm.conf
+iocage exec organizr echo 'listen.group = www' >> /usr/local/etc/php-fpm.conf
+iocage exec organizr echo 'listen.mode = 0660' >> /usr/local/etc/php-fpm.conf
 
 # Update the PHP ini
 iocage exec organizr cp /usr/local/etc/php.ini-production /usr/local/etc/php.ini
