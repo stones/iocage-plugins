8fbf49a8ddd0277620bd4c6bf4b2902c0ba4bcc4
Tom Stones <tom@tomstones.com.au>
2018-12-29 23:08:44 +1100

    UPDATED | php-fpm settings

--- organizr/setup.sh
+++ organizr/setup.sh
@@ -6,10 +6,10 @@ iocage exec organizr mkdir -p /mnt/config
 iocage fstab -a organizr /mnt/Tank/configs/organizr /mnt/config nullfs rw 0 0
 
 # Update the php fpm config
-iocage exec organizr sed -i '' -e 's?listen = 127.0.0.1:9000?listen = /var/run/php-fpm.sock?g' /usr/local/etc/php-fpm.conf
-iocage exec organizr sed -i '' -e 's/;listen.owner = www/listen.owner = www/g' /usr/local/etc/php-fpm.conf
-iocage exec organizr sed -i '' -e 's/;listen.group = www/listen.group = www/g' /usr/local/etc/php-fpm.conf
-iocage exec organizr sed -i '' -e 's/;listen.mode = 0660/listen.mode = 0600/g' /usr/local/etc/php-fpm.conf
+iocage exec organizr echo 'listen=/var/run/php-fpm.sock' >> /usr/local/etc/php-fpm.conf
+iocage exec organizr echo 'listen.owner=www' >> /usr/local/etc/php-fpm.conf
+iocage exec organizr echo 'listen.group=www' >> /usr/local/etc/php-fpm.conf
+iocage exec organizr echo 'listen.mode=0660' >> /usr/local/etc/php-fpm.conf
 
 # Update the PHP ini
 iocage exec organizr cp /usr/local/etc/php.ini-production /usr/local/etc/php.ini
