07f7cf2357e2ff2abfe8167af39de8c02bb3acf9
Tom Stones <tom@tomstones.com.au.com.au>
2018-12-30 14:47:50 +1100

    FIXED | Permissions issue with nzbget

--- nzbget/setup.sh
+++ nzbget/setup.sh
@@ -1,9 +1,9 @@
-# Create a temporary json config for iocage
-echo '{"pkgs":["ca_root_nss","wget","vim-tiny"]}' > /tmp/pkg.json
 # Create the jail
-iocage create -n "nzbget" -p /tmp/pkg.json -r 11.2-RELEASE ip4_addr="vnet0|192.168.1.57/24" defaultrouter="192.168.1.1" vnet="on" allow_raw_sockets="1" boot="on" 
-# Remove the temporary configuration
-rm /tmp/pkg.json
+iocage create -n "nzbget" -p ./pkg.json -r 11.2-RELEASE ip4_addr="vnet0|192.168.1.57/24" defaultrouter="192.168.1.1" vnet="on" allow_raw_sockets="1" boot="on" 
+# Add media user
+iocage exec nzbget "pw user add media -c media -u 8675309 -d /nonexistent -s /usr/bin/nologin"
+iocage exec nzbget "pw groupadd -n media -g 8675309"
+iocage exec nzbget "pw groupmod media -m nzbget"
 # Ensure the rc.d folder exists
 iocage exec nzbget mkdir -p /usr/local/etc/rc.d
 # Copy the nzbget file so we can use 'service nzbget start|stop|etc'
@@ -19,7 +19,10 @@ iocage exec nzbget "fetch https://nzbget.net/download/nzbget-latest-bin-freebsd.
 iocage exec nzbget "sh nzbget-latest-bin-freebsd.run --destdir /usr/local/share/nzbget"
 # remove install script
 iocage exec nzbget rm nzbget-latest-bin-freebsd.run 
+# Set permissions on mounted folders 
+iocage exec nzbget chown -R media:media /mnt/transfer /mnt/config /usr/local/share/nzbget
 # Enable nzbget
+iocage exec nzbget sysrc "nzbget_user=media"
 iocage exec nzbget sysrc "nzbget_enable=YES"
 # Restart the jail
 iocage restart nzbget
