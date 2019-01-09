c29f9e7c9a466c15a02fe1a2959adb7806256b80
Tom Stones <tom@tomstones.com.au>
2019-01-09 16:33:05 +1100

    ADDED | files

---
+++ calibre/README.md
@@ -0,0 +1,20 @@
+# [Sonarr](https://sonarr.tv/)
+
+> Sonarr is a PVR for Usenet and BitTorrent users. It can monitor multiple RSS feeds for new episodes of your favorite shows and will grab, sort and rename them. It can also be configured to automatically upgrade the quality of files already downloaded when a better quality format becomes available. 
+
+## Overview
+
+This will use the official setup script to install _nzbget_ and add 2 mount points: 
+
+- config
+- library
+- transfer
+
+## Usage
+
+Edit the `setup.sh` script to match your requirements ( the file is heavily commented ), then run:
+
+`./setup.sh`
+
+
+
---
+++ calibre/setup.sh
@@ -0,0 +1,29 @@
+# Create jail
+iocage create -n "calibre" -p ./pkg.json -r 11.2-RELEASE ip4_addr="vnet0|192.168.1.54/24" defaultrouter="192.168.1.1" vnet="on" allow_raw_sockets="1" boot="on"
+
+# Add media user
+# iocage exec sonarr "pw user add media -c media -u 8675309 -d /nonexistent -s /usr/bin/nologin"
+# iocage exec sonarr "pw groupadd -n media -g 8675309"
+# iocage exec sonarr "pw groupmod media -m sonarr"
+
+# iocage exec sonarr mkdir /usr/local/etc/rc.d
+# cp ./sonarr.rc  /mnt/iocage/jails/sonarr/root/usr/local/etc/rc.d/sonarr
+# iocage exec sonarr chmod 555 /usr/local/etc/rc.d/sonarr
+
+# # Mount folders
+# iocage fstab -a sonarr /mnt/Tank/configs/sonarr /mnt/config nullfs rw 0 0
+# iocage fstab -a sonarr /mnt/Tank/transfer/complete/television /mnt/transfer nullfs rw 0 0
+# iocage fstab -a sonarr /mnt/Tank/library/video/tv /mnt/library nullfs rw 0 0
+
+# iocage exec sonarr ln -s /usr/local/bin/mono /usr/bin/mono
+
+# # Install sonarr
+# iocage exec sonarr "fetch http://download.sonarr.tv/v2/master/mono/NzbDrone.master.tar.gz -o /usr/local/share"
+# iocage exec sonarr "tar -xzvf /usr/local/share/NzbDrone.master.tar.gz -C /usr/local/share"
+# iocage exec sonarr rm /usr/local/share/NzbDrone.master.tar.gz
+
+# iocage exec sonarr chown -R media:media /usr/local/share/NzbDrone /mnt/config
+
+# # Initialise the service
+# iocage exec sonarr sysrc "sonarr_enable=YES"
+# iocage exec sonarr service sonarr start
