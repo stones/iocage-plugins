877e5f3cc3d4261019747b19187b3a95584440da
Tom Stones <tom@tomstones.com.au>
2019-06-05 22:04:40 +1000

    UPDATED | removed braces

--- paperless/setup.sh
+++ paperless/setup.sh
@@ -6,41 +6,41 @@ JAIL_NAME="paperless"
 RELEASE=$(freebsd-version | sed "s/STABLE/RELEASE/g")
 
 # Create the jail
-iocage create -n "{$JAIL_NAME}" -p ./pkg.json -r "${RELEASE}" ip4_addr="{$JAIL_INTERFACE}|{$JAIL_IP}/24" defaultrouter="{$DEFAULT_ROUTER}" vnet="on" allow_raw_sockets="1" boot="on" 
+iocage create -n "$JAIL_NAME" -p ./pkg.json -r "${RELEASE}" ip4_addr="$JAIL_INTERFACE|$JAIL_IP/24" defaultrouter="$DEFAULT_ROUTER" vnet="on" allow_raw_sockets="1" boot="on" 
 
 # Add config folder
-iocage exec "{$JAIL_NAME}" mkdir -p /mnt/config
-iocage fstab -a "{$JAIL_NAME}" /mnt/Tank/configs/paperless /mnt/config nullfs rw 0 0
+iocage exec "$JAIL_NAME" mkdir -p /mnt/config
+iocage fstab -a "$JAIL_NAME" /mnt/Tank/configs/paperless /mnt/config nullfs rw 0 0
 
 # Pull down latest source
-iocage exec "{$JAIL_NAME}" git clone --depth 1 git clone https://github.com/the-paperless-project/paperless.git /usr/local/www/paperless
+iocage exec "$JAIL_NAME" git clone --depth 1 git clone https://github.com/the-paperless-project/paperless.git /usr/local/www/paperless
 
-iocage exec "{$JAIL_NAME}" cp /usr/local/www/paperless/paperless.conf.example /etc/paperless.conf
+iocage exec "$JAIL_NAME" cp /usr/local/www/paperless/paperless.conf.example /etc/paperless.conf
 
 # Update the config
-iocage exec "{$JAIL_NAME}" sed -i '' -e 's?PAPERLESS_CONSUMPTION_DIR=""?PAPERLESS_CONSUMPTION_DIR=/mnt/config/consumption?g' /etc/paperless.conf
-iocage exec "{$JAIL_NAME}" sed -i '' -e 's?#PAPERLESS_DBDIR=/path/to/database/file?PAPERLESS_DBDIR=/mnt/config/db?g' /etc/paperless.conf
-iocage exec "{$JAIL_NAME}" sed -i '' -e 's?#PAPERLESS_MEDIADIR=/path/to/media?PAPERLESS_MEDIADIR=/mnt/config/media?g' /etc/paperless.conf
+iocage exec "$JAIL_NAME" sed -i '' -e 's?PAPERLESS_CONSUMPTION_DIR=""?PAPERLESS_CONSUMPTION_DIR=/mnt/config/consumption?g' /etc/paperless.conf
+iocage exec "$JAIL_NAME" sed -i '' -e 's?#PAPERLESS_DBDIR=/path/to/database/file?PAPERLESS_DBDIR=/mnt/config/db?g' /etc/paperless.conf
+iocage exec "$JAIL_NAME" sed -i '' -e 's?#PAPERLESS_MEDIADIR=/path/to/media?PAPERLESS_MEDIADIR=/mnt/config/media?g' /etc/paperless.conf
 
 # Set permissions
-iocage exec "{$JAIL_NAME}" chown -R www:www /usr/local/www /mnt/config
+iocage exec "$JAIL_NAME" chown -R www:www /usr/local/www /mnt/config
 
-iocage exec "{$JAIL_NAME}" ln -s /usr/local/bin/python3 /usr/local/bin/python
+iocage exec "$JAIL_NAME" ln -s /usr/local/bin/python3 /usr/local/bin/python
 
 # Install dependencies
-iocage exec "{$JAIL_NAME}" pip-3.6 install --upgrade pip
-iocage exec "{$JAIL_NAME}" pip install --requirement /usr/local/www/paperless/requirements.txt
+iocage exec "$JAIL_NAME" pip-3.6 install --upgrade pip
+iocage exec "$JAIL_NAME" pip install --requirement /usr/local/www/paperless/requirements.txt
 
-iocage exec "{$JAIL_NAME}" /usr/local/www/paperless/src/manage.py migrate
+iocage exec "$JAIL_NAME" /usr/local/www/paperless/src/manage.py migrate
 
-cp ./nzbget.rc  "/mnt/iocage/jails/{$JAIL_NAME}/root/etc/rc.d/paperless"
+cp ./nzbget.rc  "/mnt/iocage/jails/$JAIL_NAME/root/etc/rc.d/paperless"
 # Copy site configuration
-cp ./site.nginx  "{$JAIL_FOLDER}/{$JAIL_NAME}/root/usr/local/etc/nginx/nginx.conf"
+cp ./site.nginx  "$JAIL_FOLDER/$JAIL_NAME/root/usr/local/etc/nginx/nginx.conf"
 
 # Add php and nginx to services and start up
-iocage exec "{$JAIL_NAME}" sysrc nginx_enable=YES
-iocage exec "{$JAIL_NAME}" service nginx start
+iocage exec "$JAIL_NAME" sysrc nginx_enable=YES
+iocage exec "$JAIL_NAME" service nginx start
 
-iocage exec "{$JAIL_NAME}" sysrc paperless_enable=YES
-iocage exec "{$JAIL_NAME}" service paperless start
-iocage restart "{$JAIL_NAME}"
+iocage exec "$JAIL_NAME" sysrc paperless_enable=YES
+iocage exec "$JAIL_NAME" service paperless start
+iocage restart "$JAIL_NAME"
