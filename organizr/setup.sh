ada5ef83f75528686e3b083b2fbaa877c0e36bad
Tom Stones <tom@tomstones.com.au>
2019-12-31 22:50:03 +1100

    UPDATED | Organizr

--- organizr/setup.sh
+++ organizr/setup.sh
@@ -1,32 +1,48 @@
+#!/bin/sh
+JAIL_IP="192.168.20.221"
+ROUTER="192.168.20.1"
+RELEASE=$(freebsd-version | sed "s/STABLE/RELEASE/g")
+JAIL_NAME="organizr"    
+INTERFACE="vnet0"
+
+LOCAL_CONFIG="/mnt/Tank/configs/organizr"
+JAIL_CONFIG="/mnt/config"
+
+JAILS_FOLDER="/mnt/iocage/jails"
+
 # Create the jail
-iocage create -n "organizr" -p ./pkg.json -r 11.2-RELEASE ip4_addr="vnet0|192.168.1.60/24" defaultrouter="192.168.1.1" vnet="on" allow_raw_sockets="1" boot="on" 
+if ! iocage create --name "${JAIL_NAME}" -p ./pkg.json -r "${RELEASE}" ip4_addr="${INTERFACE}|${JAIL_IP}/24" defaultrouter="${ROUTER}" boot="on" host_hostname="${JAIL_NAME}" vnet="on"
+then
+	echo "Failed to create ${JAIL_NAME}"
+	exit 1
+fi
 
 # Add config folder
-iocage exec organizr mkdir -p /mnt/config
-iocage fstab -a organizr /mnt/Tank/configs/organizr /mnt/config nullfs rw 0 0
+iocage exec "$JAIL_NAME" mkdir -p "$JAIL_CONFIG"
+iocage fstab -a "$JAIL_NAME" "$LOCAL_CONFIG" "$JAIL_CONFIG" nullfs rw 0 0
 
 # Update the php fpm config
-iocage exec organizr "echo 'listen=/var/run/php-fpm.sock' >> /usr/local/etc/php-fpm.conf"
-iocage exec organizr "echo 'listen.owner=www' >> /usr/local/etc/php-fpm.conf"
-iocage exec organizr "echo 'listen.group=www' >> /usr/local/etc/php-fpm.conf"
-iocage exec organizr "echo 'listen.mode=0660' >> /usr/local/etc/php-fpm.conf"
+iocage exec "$JAIL_NAME" "echo 'listen=/var/run/php-fpm.sock' >> /usr/local/etc/php-fpm.conf"
+iocage exec "$JAIL_NAME" "echo 'listen.owner=www' >> /usr/local/etc/php-fpm.conf"
+iocage exec "$JAIL_NAME" "echo 'listen.group=www' >> /usr/local/etc/php-fpm.conf"
+iocage exec "$JAIL_NAME" "echo 'listen.mode=0660' >> /usr/local/etc/php-fpm.conf"
 
 # Update the PHP ini
-iocage exec organizr cp /usr/local/etc/php.ini-production /usr/local/etc/php.ini
-iocage exec organizr sed -i '' -e 's?;date.timezone =?date.timezone = "Universal"?g' /usr/local/etc/php.ini
-iocage exec organizr sed -i '' -e 's?;cgi.fix_pathinfo=1?cgi.fix_pathinfo=0?g' /usr/local/etc/php.ini
+iocage exec "$JAIL_NAME" cp /usr/local/etc/php.ini-production /usr/local/etc/php.ini
+iocage exec "$JAIL_NAME" sed -i '' -e 's?;date.timezone =?date.timezone = "Universal"?g' /usr/local/etc/php.ini
+iocage exec "$JAIL_NAME" sed -i '' -e 's?;cgi.fix_pathinfo=1?cgi.fix_pathinfo=0?g' /usr/local/etc/php.ini
 
 # Pull down latest source
-iocage exec organizr git clone --depth 1 -b v2-develop https://github.com/causefx/Organizr /usr/local/www/Organizr
+iocage exec "$JAIL_NAME" git clone --depth 1 -b v2-develop https://github.com/causefx/Organizr /usr/local/www/Organizr
 
 # Set permissions
-iocage exec organizr chown -R www:www /usr/local/www /mnt/config
+iocage exec "$JAIL_NAME" chown -R www:www /usr/local/www "$JAIL_CONFIG"
 
 # Copy site configuration
-cp ./site.nginx  /mnt/iocage/jails/organizr/root/usr/local/etc/nginx/nginx.conf  
+cp ./site.nginx  "$JAILS_FOLDER/$JAIL_NAME/root/usr/local/etc/nginx/nginx.conf"
 
 # Add php and nginx to services and start up
-iocage exec organizr sysrc nginx_enable=YES
-iocage exec organizr sysrc php_fpm_enable=YES
-iocage exec organizr service nginx start
-iocage exec organizr service php-fpm start
+iocage exec "$JAIL_NAME" sysrc nginx_enable=YES
+iocage exec "$JAIL_NAME" sysrc php_fpm_enable=YES
+iocage exec "$JAIL_NAME" service nginx start
+iocage exec "$JAIL_NAME" service php-fpm start
