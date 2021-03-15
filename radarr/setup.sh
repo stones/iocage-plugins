3cdd83dc9bfe069fcffbbeee52a70c6aaab26b0a
Tom Stones <tom@tomstones.com.au.com.au>
2021-03-15 22:48:21 +1100

    UPDATED |  Radarr. It's needs more

--- radarr/setup.sh
+++ radarr/setup.sh
@@ -1,5 +1,11 @@
+RELEASE=$(freebsd-version | cut -d - -f -1)"-RELEASE"
+JAILS_MOUNT=$(zfs get -H -o value mountpoint $(iocage get -p)/iocage)
+
+echo "$JAIL_MOUNT"
+
+
 # Create Jail
-iocage create -n "radarr" -p ./pkg.json -r 11.2-RELEASE ip4_addr="vnet0|192.168.1.55/24" defaultrouter="192.168.1.1" vnet="on" allow_raw_sockets="1" boot="on"
+iocage create -n "radarr" -p ./pkg.json -r "$RELEASE" ip4_addr="vnet0|192.168.20.211/24" defaultrouter="192.168.20.1" vnet="on" allow_raw_sockets="1" boot="on"
 
 # Update to Latest Repo
 iocage exec radarr "mkdir -p /usr/local/etc/pkg/repos"
@@ -10,6 +16,15 @@ iocage exec radarr "pkg upgrade"
 # Install pkgs
 # iocage exec radarr pkg update && pkg upgrade
 
+
+#fetch https://github.com/jailmanager/jailmanager.github.io/releases/download/v0.0.1/mono-6.8.0.105.txz
+#pkg install -y mono-6.8.0.105.txz
+
+
+iocage exec radarr mkdir /mnt/config
+iocage exec radarr mkdir /mnt/transfer
+iocage exec radarr mkdir /mnt/library
+
 iocage fstab -a radarr /mnt/Tank/configs/radarr /mnt/config nullfs rw 0 0
 iocage fstab -a radarr /mnt/Tank/transfer/complete/movies /mnt/transfer nullfs rw 0 0
 iocage fstab -a radarr /mnt/Tank/library/video/movies /mnt/library nullfs rw 0 0
@@ -18,9 +33,9 @@ iocage fstab -a radarr /mnt/Tank/library/video/movies /mnt/library nullfs rw 0 0
 iocage exec radarr ln -s /usr/local/bin/mono /usr/bin/mono
 
 # Download radarr
-iocage exec radarr "fetch https://github.com/Radarr/Radarr/releases/download/v0.2.0.1217/Radarr.v0.2.0.1217.linux.tar.gz -o /usr/local/share"
-iocage exec radarr "tar -xzvf /usr/local/share/Radarr.v0.2.0.1217.linux.tar.gz -C /usr/local/share"
-iocage exec radarr rm /usr/local/share/Radarr.v0.2.0.1217.linux.tar.gz
+iocage exec radarr "fetch https://github.com/Radarr/Radarr/releases/download/v3.0.2.4552/Radarr.master.3.0.2.4552.linux.tar.gz -o /usr/local/share"
+iocage exec radarr "tar -xzvf /usr/local/share/Radarr.master.3.0.2.4552.linux.tar.gz -C /usr/local/share"
+iocage exec radarr rm /usr/local/share/Radarr.master.3.0.2.4552.linux.tar.gz
 
 ## Media Permissions
 iocage exec radarr "pw user add radarr -c radarr -u 352 -d /nonexistent -s /usr/bin/nologin"
@@ -33,7 +48,7 @@ iocage exec radarr chown -R media:media /usr/local/share/Radarr /mnt/config
 
 # Copy sysrc
 iocage exec radarr mkdir -p /usr/local/etc/rc.d
-cp ./radarr.rc  /mnt/iocage/jails/radarr/root/usr/local/etc/rc.d/radarr
+cp ./radarr.rc  /mnt/Tank/iocage/jails/radarr/root/usr/local/etc/rc.d/radarr
 iocage exec radarr chmod u+x /usr/local/etc/rc.d/radarr
 
 iocage exec radarr sysrc "radarr_enable=YES"
